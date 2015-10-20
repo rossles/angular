785b77c2b98084b836836e4692b59a1fb17a6040
Maarten Rozie <rossles@Maartens-iMac.local>
2015-10-20 21:43:19 +0200

    check

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testerApp
+ */
+angular.module('testerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
