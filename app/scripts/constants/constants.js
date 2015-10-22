4b95ba1a4fa87c309ef4e74ab2ec0fe7bbc74645
maarten rozie <rossles@gmail.com>
2015-10-22 14:36:45 +0200

    jslint update

--- app/scripts/constants/constants.js
+++ app/scripts/constants/constants.js
@@ -1,9 +1,11 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular.module('testerApp')
-    .constant('LAST_FM', {
-        'URL': 'http://ws.audioscrobbler.com/2.0/',
-        'FORMAT': 'json',
-        'USER': '_USER_',
-        'API_KEY': '_API_KEY_'
-    });
+    angular.module('testerApp')
+        .constant('LAST_FM', {
+            'URL': 'http://ws.audioscrobbler.com/2.0/',
+            'FORMAT': 'json',
+            'USER': '_USER_',
+            'API_KEY': '_API_KEY_'
+        });
+}());
